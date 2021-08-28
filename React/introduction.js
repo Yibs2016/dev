bd5304f6da4752bf74126557b585b9ea4c3ed492
Yibs2016 <yeyeyezi@163.com>
2021-08-28 15:12:39 +0800

    feat: my react note

---
+++ React/introduction.js
@@ -0,0 +1,37 @@
+//简洁
+  //专注视图层
+  //virtual dom 其他平台集成
+  //函数式编程 简单函数，易于测试
+ 
+//jsx
+  //类xml语法的ecma script扩展 js写html React.createElement dom元素+自定义元素 
+  //React将所有dom的字符串转义，防止xss
+//组件
+	//封装性
+	//生命周期
+	//数据流动
+	//分层思想
+	//web compontents
+	  //html template, custom elements, shadow dom, html imports
+	//react组件 理念与web comp一致
+		//继承顶层类React.Compontent(声明props, context, refs...,setState, forceUpdate)
+	  //props state 生命周期
+		//将组件拆分到合理粒度，组合成业务组件
+	//类组件(状态)，函数式组件(无状态，优化检查和内存分配)
+
+//数据流
+//生命周期
+//dom
+
+//事件系统
+//组件通信
+//组件抽象
+//性能优化
+//自动化测试
+
+//源码
+
+//flux
+//redux
+//服务器渲染
+//可视化
